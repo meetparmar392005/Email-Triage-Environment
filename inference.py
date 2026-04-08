7210ee26b4aba52d6bca219219987ed5991c35d0
Dhruval Patel <dvp2122005@gmail.com>
2026-04-08 15:07:15 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -22,8 +22,8 @@ from email_triage_env import EmailAction, EmailTriageEnv
 
 API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
 MODEL_NAME = os.getenv("MODEL_NAME", "Qwen/Qwen2.5-72B-Instruct")
-# HF_TOKEN = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
-HF_TOKEN = "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"
+HF_TOKEN = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
+
 
 LOCAL_IMAGE_NAME = os.getenv("LOCAL_IMAGE_NAME")
 
